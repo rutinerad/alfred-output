e9326eb08a9d0f8804c3c6de454aa12b5ca1a688
Samuel Lindblom <samuel.lindblom@paf.com>
2018-11-22 12:26:13 +0200

    revert static method for creating alfred items object

--- src/AlfredItems.php
+++ src/AlfredItems.php
@@ -23,12 +23,6 @@ class AlfredItems {
 
         return json_encode($items);
 	}
-
-	static function of(array $items) {
-		$object = new AlfredItems();
-		$object->addItems($items);
-		return $object;
-	}
 }
 
 // {"items": [
